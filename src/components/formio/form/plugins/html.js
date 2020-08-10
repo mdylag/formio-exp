393bd0025dbeb0760af69ef026289c3ffcdd51f5
aerdukant <arek.erduk@gmail.com>
2020-08-10 12:48:43 +0200

    Commented out setters for owner id, submission id and modify fields

--- src/components/formio/form/plugins/html.js
+++ src/components/formio/form/plugins/html.js
@@ -8,37 +8,37 @@ export default (element, component) => {
       id: Math.random().toString(36).substring(7)
     });
 
-    componentElement.appendChild(FormioExportUtils.createElement('h1', { class: 'form-title' }, component.title));
+    componentElement.appendChild(FormioExportUtils.createElement('h2', { class: 'form-title' }, component.title));
 
-    if (component._options.submission) {
-      if (component._options.submission.hasOwnProperty('owner')) {
-        componentElement.appendChild(FormioExportUtils.createElement('div', { class: 'row' },
-          FormioExportUtils.createElement('div', { class: 'col text-right text-bold' }, 'submission owner:'),
-          FormioExportUtils.createElement('div', { class: 'col text-left' }, component._options.submission.owner))
-        );
-      }
+    // if (component._options.submission) {
+    // if (component._options.submission.hasOwnProperty('owner')) {
+    //   componentElement.appendChild(FormioExportUtils.createElement('div', { class: 'row' },
+    //     FormioExportUtils.createElement('div', { class: 'col text-right text-bold' }, 'submission owner:'),
+    //     FormioExportUtils.createElement('div', { class: 'col text-left' }, component._options.submission.owner))
+    //   );
+    // }
+    //
+    // if (component._options.submission.hasOwnProperty('id')) {
+    //   componentElement.appendChild(FormioExportUtils.createElement('div', { class: 'row' },
+    //     FormioExportUtils.createElement('div', { class: 'col text-right text-bold' }, 'submission id:'),
+    //     FormioExportUtils.createElement('div', { class: 'col text-left' }, component._options.submission.id))
+    //   );
+    // }
 
-      if (component._options.submission.hasOwnProperty('id')) {
-        componentElement.appendChild(FormioExportUtils.createElement('div', { class: 'row' },
-          FormioExportUtils.createElement('div', { class: 'col text-right text-bold' }, 'submission id:'),
-          FormioExportUtils.createElement('div', { class: 'col text-left' }, component._options.submission.id))
-        );
-      }
-
-      if (component._options.submission.hasOwnProperty('modified')) {
-
-        let date = '';
-
-        if (component._options.submission.modified && component._options.submission.modified) {
-          date = component._options.submission.modified.replace('T', ' ').split('.')[0] + ' UTC';
-        }
-
-        componentElement.appendChild(FormioExportUtils.createElement('div', { class: 'row' },
-          FormioExportUtils.createElement('div', { class: 'col text-right text-bold' }, 'modified:'),
-          FormioExportUtils.createElement('div', { class: 'col text-left' }, date)
-        ));
-      }
-    }
+    // if (component._options.submission.hasOwnProperty('modified')) {
+    //
+    //   let date = '';
+    //
+    //   if (component._options.submission.modified && component._options.submission.modified) {
+    //     date = component._options.submission.modified.replace('T', ' ').split('.')[0] + ' UTC';
+    //   }
+    //
+    //   componentElement.appendChild(FormioExportUtils.createElement('div', { class: 'row' },
+    //     FormioExportUtils.createElement('div', { class: 'col text-right text-bold' }, 'modified:'),
+    //     FormioExportUtils.createElement('div', { class: 'col text-left' }, date)
+    //   ));
+    // }
+    // }
 
     componentElement.appendChild(FormioExportUtils.createElement('br'));
 
