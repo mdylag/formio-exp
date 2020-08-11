cd92ae0ad663b273f4addd8f0f994ab36bf352b8
aerdukant <arek.erduk@gmail.com>
2020-08-11 23:05:16 +0200

    Table fixes

--- src/components/formio/table/plugins/html.js
+++ src/components/formio/table/plugins/html.js
@@ -4,25 +4,35 @@ import FormioExportUtils from '../../../../utils';
 export default (element, component) => {
 
   if (component) {
-
-    let componentElement = FormioExportUtils.createElement('table', {
-      class: 'table',
+    let componentElement = FormioExportUtils.createElement('div', {
+      class: `formio-component ${component.type}-component card after`,
+      style: 'border-collapse: collapse;',
       id: Math.random().toString(36).substring(7)
     });
 
-    componentElement.className += component._options.bordered ? ' table-bordered' : '';
+    let tableComponent = FormioExportUtils.createElement('table', {
+      class: 'table',
+      style: 'border-collapse: collapse;',
+      id: Math.random().toString(36).substring(7)
+    });
+
+    tableComponent.className += component._options.bordered ? ' table-bordered' : '';
 
     console.log('ftable', component.rows);
 
     _.forEach(component.rows, (row) => {
 
-      let rowComponent = FormioExportUtils.createElement('tr', { });
+      let rowComponent = FormioExportUtils.createElement('tr', {
+        style: 'border-collapse: collapse;'
+      });
 
       console.log('frow', row);
 
       _.forEach(row, (comp) => {
 
-        let columnComponent = FormioExportUtils.createElement('td', { });
+        let columnComponent = FormioExportUtils.createElement('td', {
+          class: 'cellBorder'
+        });
 
         _.forEach(comp.components, (c) => {
 
@@ -34,9 +44,9 @@ export default (element, component) => {
         rowComponent.appendChild(columnComponent);
 
       });
-      componentElement.appendChild(rowComponent);
+      tableComponent.appendChild(rowComponent);
     });
-
+    componentElement.append(tableComponent);
     if (_.isElement(element)) {
       element.appendChild(componentElement);
     }
